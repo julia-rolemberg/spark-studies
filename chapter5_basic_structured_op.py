5cc6e72fd12c5172f41ba1ab8f76a63979fc7b5e
julia-rolemberg <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>
2022-06-17 11:29:36 -0300

    chapter 5

---
+++ chapter5_basic_structured_op.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, LongType
+from pyspark.sql.functions import expr, col
+
+spark = SparkSession \
+    .builder \
+    .appName("Basic Structured Operations") \
+    .getOrCreate()
+    
+df = spark.read.format("json").load("data/2015-summary.json")
+
+df.printSchema()
+
+#testing manual  schema possible problemas
+myManualSchema = StructType([
+  StructField("DEST_COUNTRY_NAME", StringType(), True),
+  StructField("ORIGIN_COUNTRY_NAME", StringType(), True),
+  StructField("count", LongType(), False, metadata={"hello":"world"})
+])
+df = spark.read.format("json").schema(myManualSchema)\
+  .load("/data/2015-summary.json")
+
+# expr allow us to write a transformation directly in a column
+expr("(((someCol + 5) * 200) - 6) < otherCol")
+
+# doing the same thing with columns would be like
+df.withColumn("newCol", (((col("someCol")+ 5)*200 -6)< col("otherCol")))
+
