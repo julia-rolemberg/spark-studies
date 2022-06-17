79e5dc5683e552b16aed74d85ab6261a14d5d91b
julia-rolemberg <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>
2022-06-17 11:38:43 -0300

    adjusts

--- chapter5_basic_structured_op.py
+++ chapter5_basic_structured_op.py
@@ -18,11 +18,10 @@ myManualSchema = StructType([
   StructField("count", LongType(), False, metadata={"hello":"world"})
 ])
 df = spark.read.format("json").schema(myManualSchema)\
-  .load("/data/2015-summary.json")
+  .load("data/2015-summary.json")
 
 # expr allow us to write a transformation directly in a column
 expr("(((someCol + 5) * 200) - 6) < otherCol")
 
 # doing the same thing with columns would be like
-df.withColumn("newCol", (((col("someCol")+ 5)*200 -6)< col("otherCol")))
-
+c
